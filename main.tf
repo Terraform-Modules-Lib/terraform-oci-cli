2c263106d2f4f5b00722763b664f0bc174e80bf1
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2022-08-24 12:23:36 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,6 +27,7 @@ data "http" "oci_cli_installer" {
 
 resource "local_file" "oci_cli_installer" {
   filename = "./oci_cli_installer.sh"
+  
   content = data.http.oci_cli_installer.response_body
 }
 
@@ -49,4 +50,16 @@ resource "local_sensitive_file" "oci_cli_private_key" {
   
   filename = "./.oci/oci.key"
   content = var.oci_private_key
+}
+
+resource "local_file" "oci_cli_config_file" {
+  filename = "./.oci/config"
+  
+  content = templatefile("./oci_config_file.tftpl", {
+    oci_tenancy_ocid = var.oci_tenancy_id
+    oci_region = var.oci_region_name
+    oci_user_ocid = var.oci_user_id
+    oci_key_fingerprint = var.oci_key_fingerprint
+    oci_private_key_path = local_sensitive_file.oci_cli_private_key.filename
+  })
 }
