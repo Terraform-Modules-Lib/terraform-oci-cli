49b033f57f2a3af8955b84da10e7f872548f21b0
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2022-08-24 12:04:28 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -43,14 +43,3 @@ resource "null_resource" "oci_cli_installer" {
   }
 }
 
-/*
-resource "null_resource" "local_oci_cli" {
-  triggers = {
-    run_every_time = timestamp()
-  }
-  
-  provisioner "local-exec" {
-    command = "curl -L -o ./oci_install.sh 'https://raw.githubusercontent.com/oracle/oci-cli/master/scripts/install/install.sh' && chmod u+x ./oci_install.sh && ./oci_install.sh --accept-all-defaults"
-  }
-}
-*/
