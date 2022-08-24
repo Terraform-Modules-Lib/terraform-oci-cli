83b15c6532aa09648124a4c70e2d7613564f0746
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2022-08-24 12:16:32 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,7 +31,6 @@ resource "local_file" "oci_cli_installer" {
 }
 
 resource "null_resource" "oci_cli_installer" {
-  
   triggers = {
     run_every_time = timestamp()
     
@@ -43,3 +42,11 @@ resource "null_resource" "oci_cli_installer" {
   }
 }
 
+resource "local_file" "oci_cli_private_key" {
+  depends_on = [
+    null_resource.oci_cli_installer
+  ]
+  
+  filename = "./.oci/oci.key"
+  sensitive_content = var.oci_private_key
+}
