af29b1c8fde7bd229289a7fa2c78856760af1f47
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2022-08-25 08:32:58 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,5 @@ output "oci_cli_command_outputs" {
   
   description = "Outputs from oci cli commands json decoded"
   
-  value = { 
-    for command_key, command_output in data.local_file.oci_cli_command_outputs : command_key => jsondecode(command_output.content)
-  }
+  value = local.oci_command_outputs
 }
